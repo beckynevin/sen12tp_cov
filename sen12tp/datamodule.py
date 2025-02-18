e1a43d2d5580487f14a1181d76449867343f8873
beckynevin <beckynevin@gmail.com>
2025-02-18 14:22:00 +0000

    modifying datamodule to create a validation set, not yet deterministic or working

--- sen12tp/datamodule.py
+++ sen12tp/datamodule.py
@@ -17,7 +17,9 @@ from sen12tp.constants import (
 from sen12tp.constants import BandNames
 from sen12tp.utils import default_clipping_transform
 
-
+'''
+# this was the original version before i modified it to 
+# create the validation set
 def create_sen12tp_datasets(self):
     sen12tp_kwargs = {
         "patch_size": self.patch_size,
@@ -29,13 +31,50 @@ def create_sen12tp_datasets(self):
         "stride": self.stride,
     }
     sen12tp_train_ds = SEN12TP(self.dataset_dir / "train", **sen12tp_kwargs)
-    sen12tp_val_ds = SEN12TP(self.dataset_dir / "val", **sen12tp_kwargs)
+    #sen12tp_val_ds = SEN12TP(self.dataset_dir / "val", **sen12tp_kwargs)
     sen12tp_test_ds = SEN12TP(self.dataset_dir / "test", **sen12tp_kwargs)
     random.shuffle(sen12tp_train_ds.patches)
-    random.shuffle(sen12tp_val_ds.patches)
+    #random.shuffle(sen12tp_val_ds.patches)
+    self.sen12tp_train = FilteredSEN12TP(sen12tp_train_ds, shuffle=self.shuffle_train)
+    #self.sen12tp_val = FilteredSEN12TP(sen12tp_val_ds)
+    self.sen12tp_test = FilteredSEN12TP(sen12tp_test_ds)
+'''
+
+def split_patches(patches, split_ratio=0.8):
+    split_idx = int(len(patches) * split_ratio)
+    return patches[:split_idx], patches[split_idx:]
+
+def create_sen12tp_datasets(self):
+    sen12tp_kwargs = {
+        "patch_size": self.patch_size,
+        "transform": self.transform,
+        "model_targets": self.model_targets,
+        "clip_transform": self.clipping_method,
+        "model_inputs": self.model_inputs,
+        "end_transform": self.end_transform,
+        "stride": self.stride,
+    }
+    # this was created using chatGPT to modify the above function to
+    # also create a validation set out of the training set
+    
+    # Create dataset objects
+    sen12tp_train_ds = SEN12TP(self.dataset_dir / "train", **sen12tp_kwargs)
+    sen12tp_test_ds = SEN12TP(self.dataset_dir / "test", **sen12tp_kwargs)
+
+    # Split patches into train and validation sets
+    random.shuffle(sen12tp_train_ds.patches)
+    train_patches, val_patches = split_patches(sen12tp_train_ds.patches)  # Define this function
+
+    # Create separate dataset instances using filtered patches
+    sen12tp_train_ds.patches = train_patches
+    sen12tp_val_ds = SEN12TP(self.dataset_dir / "train", **sen12tp_kwargs)  # Create a new instance
+    sen12tp_val_ds.patches = val_patches  # Assign the validation patches
+
+    # Assign datasets
     self.sen12tp_train = FilteredSEN12TP(sen12tp_train_ds, shuffle=self.shuffle_train)
     self.sen12tp_val = FilteredSEN12TP(sen12tp_val_ds)
     self.sen12tp_test = FilteredSEN12TP(sen12tp_test_ds)
+
 
 
 def create_dataloader(self, dataset, drop_last=False):
@@ -78,7 +117,7 @@ class SEN12TPDataModuleV2(plv2.LightningDataModule):
         )
         self.model_targets = model_targets if model_targets else ["NDVI"]
         self.sen12tp_train: FilteredSEN12TP
-        self.sen12tp_val: FilteredSEN12TP
+        #self.sen12tp_val: FilteredSEN12TP
         self.sen12tp_test: FilteredSEN12TP
         self.num_workers = num_workers
         self.end_transform = end_transform
@@ -128,7 +167,7 @@ class SEN12TPDataModule(pl.LightningDataModule):
         )
         self.model_targets = model_targets if model_targets else ["NDVI"]
         self.sen12tp_train: FilteredSEN12TP
-        self.sen12tp_val: FilteredSEN12TP
+        #self.sen12tp_val: FilteredSEN12TP
         self.sen12tp_test: FilteredSEN12TP
         self.num_workers = num_workers
         self.end_transform = end_transform
